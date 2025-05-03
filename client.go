30996d150e848d4b9879d81f6d18bfa2f4fbab79
Elegant <user@elegant996.net>
2025-05-03 22:20:44 +0800

    chore: remove PostForm and PostFile functions
    not required for SCGI

--- client.go
+++ client.go
@@ -219,55 +219,6 @@ func (c *client) Post(p map[string]string, method string, bodyType string, body 
 	return c.Request(p, body)
 }
 
-// PostForm issues a POST to the scgi responder, with form
-// as a string key to a list values (url.Values)
-func (c *client) PostForm(p map[string]string, data url.Values) (resp *http.Response, err error) {
-	body := bytes.NewReader([]byte(data.Encode()))
-	return c.Post(p, "POST", "application/x-www-form-urlencoded", body, int64(body.Len()))
-}
-
-// PostFile issues a POST to the scgi responder in multipart(RFC 2046) standard,
-// with form as a string key to a list values (url.Values),
-// and/or with file as a string key to a list file path.
-func (c *client) PostFile(p map[string]string, data url.Values, file map[string]string) (resp *http.Response, err error) {
-	buf := &bytes.Buffer{}
-	writer := multipart.NewWriter(buf)
-	bodyType := writer.FormDataContentType()
-
-	for key, val := range data {
-		for _, v0 := range val {
-			err = writer.WriteField(key, v0)
-			if err != nil {
-				return
-			}
-		}
-	}
-
-	for key, val := range file {
-		fd, e := os.Open(val)
-		if e != nil {
-			return nil, e
-		}
-		defer fd.Close()
-
-		part, e := writer.CreateFormFile(key, filepath.Base(val))
-		if e != nil {
-			return nil, e
-		}
-		_, err = io.Copy(part, fd)
-		if err != nil {
-			return
-		}
-	}
-
-	err = writer.Close()
-	if err != nil {
-		return
-	}
-
-	return c.Post(p, "POST", bodyType, buf, int64(buf.Len()))
-}
-
 // SetReadTimeout sets the read timeout for future calls that read from the
 // scgi responder. A zero value for t means no timeout will be set.
 func (c *client) SetReadTimeout(t time.Duration) error {
