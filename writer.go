da16f8b90b664a7853db6fc42826f9f122161581
Elegant <user@elegant996.net>
2025-05-10 02:22:44 +0800

    restore FlushStream

--- writer.go
+++ writer.go
@@ -58,6 +58,11 @@ func (w *streamWriter) writeNetstring(pairs map[string]string) error {
 	w.buf.WriteString(sb.String())
 	w.buf.WriteByte(',')
 
+	return w.FlushStream()
+}
+
+// FlushStream flush data then end current stream
+func (w *streamWriter) FlushStream() error {
 	_, err := w.buf.WriteTo(w.c.rwc)
 	return err
 }
